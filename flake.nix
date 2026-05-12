d18ab230a761bc3579a54acc8e867507f2a67b5a
Fr4nk1in <fushen@mail.ustc.edu.cn>
2026-05-12 14:22:16 +0800

    chore: add pdf2svg to environment

--- flake.nix
+++ flake.nix
@@ -6,16 +6,19 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            typst
-          ];
-        };
-      });
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          typst
+          pdf2svg
+        ];
+      };
+    });
 }
